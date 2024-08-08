8a94612268449ed571b7e4bce0604736b1ad023a
Rikhita12 <47424764+Rikhita12@users.noreply.github.com>
2024-08-08 16:07:16 -0500

    Updating Airline Project Code

--- main.cpp
+++ main.cpp
@@ -1,339 +1,261 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-/*
-Airline flight reservation system (online booking of tickets in different flights for different destinations all over the world, cancellation of tickets, clear display of cancellation amount, refund of amount after cancellation, showing  availability of all flights, showing flights timings for all 7 days of a week, seats availability, seat selection for travelers by giving the complete layout of the seating arrangement inside the flights, food availability/non-availability inside the flights, change of travel dates and amount charged.)
-*/
 
-//To store the total number of seats in the flight
-int total_number_of_seats=100;
-//To store if seat is booked or not, seat booked = -1, seat unbooked = 0
-int seats[100] = {0};	
+// Total number of seats in the flight
+const int TOTAL_SEATS = 100;
 
-bool taken_seat = false;
+// Array to track seat booking status: -1 for booked, 0 for available
+int seatStatus[TOTAL_SEATS] = {0};    
 
-//To store the number of seats booked till now
-int reserve_seats = 1000;
+// Indicates whether a seat is already taken
+bool isSeatTaken = false;
 
-//To store the number of cancelled tickets booked till now
-int cancel_tickets = 0;
+// Counter for total seats reserved
+int totalReservedSeats = 1000;
 
-class Flight{
-	public : 
-	   Flight()
-	   {
-		  start = NULL;
-	   }
-	   void book_ticket();
-	   void cancel_ticket();
-	   void change_reservation();
-	   void passenger_details();
-	   void get_booking_details();
+// Counter for total tickets cancelled
+int totalCancelledTickets = 0;
 
-	private : 
-	   //To store details of passenger
-	   struct passenger
-	   {
-		string fname;
-		string lname;
-		string ID;
-		string phone_number;
-		string food_menu;
-		int seat_number;
-		int reservation_number;
-		passenger *next;
-	   };
-	   //To denote the start of linked list of passengers
-	   passenger *start;
+class FlightReservation {
+public:
+    FlightReservation() {
+        headPassenger = nullptr;
+    }
+    void bookTicket();
+    void cancelTicket();
+    void changeReservation();
+    void showPassengerDetails();
+    void showAllBookings();
 
-	   //Temporary pointers
-	   passenger *temp_passenger;
-	   passenger *temp1;
-}flight;	//flight is object of class Flight
+private:
+    // Structure to store passenger details
+    struct Passenger {
+        string firstName;
+        string lastName;
+        string id;
+        string phoneNumber;
+        string mealPreference;
+        int seatNumber;
+        int reservationNumber;
+        Passenger* next;
+    };
+    
+    // Pointer to the start of the linked list of passengers
+    Passenger* headPassenger;
 
-void allocate_seat_number(int snumber)
-{
-    for (int i = 0; i < total_number_of_seats; i++ )
-    {
-            if( seats[i] == -1 )
-            {
-			taken_seat=true;
-  	             cout << "The seat is taken already. \n";
-                    cout << "Please choose another seat number from below."<<endl;
-			int j = 1;
-			while ( j < total_number_of_seats+1 )
-			{
-				if ( seats[j-1] == -1)
-				j++;
-				else
-				{
-					cout <<"|" << j << "|";
-					if ( j%10 == 0 )
-					cout << endl;
-					j++;
-				}
-			}
-		}
+    // Temporary pointers for linked list traversal
+    Passenger* tempPassenger;
+    Passenger* prevPassenger;
+};
+
+void checkAvailableSeats(int seatNumber) {
+    for (int i = 0; i < TOTAL_SEATS; i++) {
+        if (seatStatus[i] == -1) {
+            isSeatTaken = true;
+            cout << "The seat is already booked. Please choose another seat from the available options:" << endl;
+            int j = 1;
+            while (j <= TOTAL_SEATS) {
+                if (seatStatus[j - 1] == -1)
+                    j++;
+                else {
+                    cout << "|" << j << "|";
+                    if (j % 10 == 0)
+                        cout << endl;
+                    j++;
+                }
+            }
+        }
     }
 }
 
-void Flight :: book_ticket()
-{
-	//To store the details of passenger
-	temp_passenger = new passenger;
-	cout << "Enter your first name: ";
-	cin >> temp_passenger->fname;
-	cout << "Enter your last name: ";
-	cin >> temp_passenger->lname;
-	cout << "Enter your ID: ";
-	cin >> temp_passenger->ID;
-	cout << "Enter your phone number: ";
-	cin >> temp_passenger->phone_number;
-	int snumber;
-	do{
-		cout<<"Enter the seat number : ";
-		cin>>snumber;
-		while(snumber>total_number_of_seats)
-		{
-			cout<<"Invalid seat number, enter again : ";
-			cin>>snumber;
-		}
-		if(seats[snumber-1]>-1)
-		{
-			taken_seat = false;
-		}
-		else 
-		allocate_seat_number(snumber);
+void FlightReservation::bookTicket() {
+    // Allocate memory for a new passenger
+    tempPassenger = new Passenger;
+    
+    // Collect passenger details
+    cout << "Enter your first name: ";
+    cin >> tempPassenger->firstName;
+    cout << "Enter your last name: ";
+    cin >> tempPassenger->lastName;
+    cout << "Enter your ID: ";
+    cin >> tempPassenger->id;
+    cout << "Enter your phone number: ";
+    cin >> tempPassenger->phoneNumber;
 
-		seats[snumber-1] = -1;
-		temp_passenger->seat_number = snumber;
+    int seatNumber;
+    do {
+        cout << "Enter the seat number: ";
+        cin >> seatNumber;
+        while (seatNumber > TOTAL_SEATS) {
+            cout << "Invalid seat number. Please enter again: ";
+            cin >> seatNumber;
+        }
+        if (seatStatus[seatNumber - 1] > -1) {
+            isSeatTaken = false;
+        } else {
+            checkAvailableSeats(seatNumber);
+        }
 
-	}while(taken_seat == true);
+        seatStatus[seatNumber - 1] = -1;
+        tempPassenger->seatNumber = seatNumber;
+    } while (isSeatTaken == true);
 
+    // Collect meal preference
+    cout << "Enter your food preference: ";
+    cout << "1. Vegetarian" << endl;
+    cout << "2. Non-Vegetarian" << endl;
+    cout << "3. No Food" << endl;
+    int choice;
+    cout << "Your choice: ";
+    cin >> choice;
+    while (choice < 1 || choice > 3) {
+        cout << "Invalid choice. Please enter again: ";
+        cin >> choice;
+    }
 
-	cout << "Enter your food choice preference : ";
-	cout<< "1. Veg" << endl;
-	cout<< "2. Non-Veg" << endl;
-	cout<< "3. No Food" << endl;
-	int choice;
-	cout<<"Your choice : ";
-	cin>>choice;
-	while(choice>3 || choice<1)
-	{
-		cout<<"Invalid choice, enter again : ";
-		cin>>choice;
-	}
+    if (choice == 1) {
+        tempPassenger->mealPreference = "Vegetarian";
+    } else if (choice == 2) {
+        tempPassenger->mealPreference = "Non-Vegetarian";
+    } else {
+        tempPassenger->mealPreference = "No Food";
+    }
+    tempPassenger->next = nullptr;
 
-	if(choice==1)
-	{
-		temp_passenger->food_menu = "Veg";
-	}
-	else if(choice==2)
-	{
-		temp_passenger->food_menu = "Non-Veg";
-	}
-	else
-	{
-		temp_passenger->food_menu = "No Food";
-	}
-	temp_passenger->next = NULL;
-	
-	reserve_seats++;
-	temp_passenger->reservation_number = reserve_seats;
-	cout<<"Your reservation number is ::== "<<reserve_seats;
+    // Assign reservation number
+    totalReservedSeats++;
+    tempPassenger->reservationNumber = totalReservedSeats;
+    cout << "Your reservation number is: " << totalReservedSeats << endl;
 
-
-	temp_passenger->next = NULL;
-
-
-	//If the linked list is empty
-	if(start == NULL)
-	{
-		start = temp_passenger;
-	}
-	else
-	{
-		passenger *temp = start;
-		while(temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = temp_passenger;
-	}
+    // Add passenger to linked list
+    if (headPassenger == nullptr) {
+        headPassenger = tempPassenger;
+    } else {
+        Passenger* current = headPassenger;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = tempPassenger;
+    }
 }
 
-void Flight :: cancel_ticket()
-{
-	int reservation_number;
-	cout << "Enter your reservation number: ";
-	cin >> reservation_number;
-	temp_passenger = start;
-	temp1 = start;
-	while(temp_passenger != NULL)
-	{
-		if(temp_passenger->reservation_number == reservation_number)
-		{
-			if(temp_passenger == start)
-			{
-				start = start->next;
-				seats[0] = 0;
-				cancel_tickets++;
-				delete temp_passenger;
-				break;
-			}
-			else
-			{
-				temp1->next = temp_passenger->next;
-				seats[temp_passenger->seat_number-1] = -1;
-				delete temp_passenger;
-				cancel_tickets++;
-				break;
-			}
-		}
-		else
-		{
-			temp1 = temp_passenger;
-			temp_passenger = temp_passenger->next;
-		}
-	}
+void FlightReservation::cancelTicket() {
+    int reservationNumber;
+    cout << "Enter your reservation number: ";
+    cin >> reservationNumber;
+    tempPassenger = headPassenger;
+    prevPassenger = headPassenger;
+    while (tempPassenger != nullptr) {
+        if (tempPassenger->reservationNumber == reservationNumber) {
+            if (tempPassenger == headPassenger) {
+                headPassenger = headPassenger->next;
+                seatStatus[tempPassenger->seatNumber - 1] = 0;
+                totalCancelledTickets++;
+                delete tempPassenger;
+                break;
+            } else {
+                prevPassenger->next = tempPassenger->next;
+                seatStatus[tempPassenger->seatNumber - 1] = 0;
+                delete tempPassenger;
+                totalCancelledTickets++;
+                break;
+            }
+        } else {
+            prevPassenger = tempPassenger;
+            tempPassenger = tempPassenger->next;
+        }
+    }
 }
 
-void Flight :: change_reservation()
-{
-	int currentseat_number , next_seat;
-	cout << " Please enter your seat number: ";
-	cin >> currentseat_number;
-	passenger *currentpass;
-	currentpass = start;
+void FlightReservation::changeReservation() {
+    int currentSeatNumber, newSeatNumber;
+    cout << "Enter your current seat number: ";
+    cin >> currentSeatNumber;
+    Passenger* currentPassenger = headPassenger;
+    
+    while (currentPassenger != nullptr) {
+        if (currentPassenger->seatNumber == currentSeatNumber) {
+            break;
+        }
+        currentPassenger = currentPassenger->next;
+    }
 
-	while(currentpass != NULL)
-	{
-		if ( currentpass->seat_number == currentseat_number)
-			break;
-		currentpass = currentpass->next;
-	}
-	cout << "Please choose another seat number from below.";
-		int x = 1;
-		while ( x < total_number_of_seats+1 )
-		{
-			if ( seats[x-1] == -1)
-			x++;
-			else
-			{
-			cout <<"| " << x << "|";
-			if ( x%10 == 0 )
-			cout << endl;
-			}
-		}
-	cin >> next_seat;
-	seats[currentpass->seat_number-1]=0;
-	currentpass->seat_number = next_seat;
-	seats[next_seat-1] = -1;
+    cout << "Choose a new seat number from the available options:" << endl;
+    int i = 1;
+    while (i <= TOTAL_SEATS) {
+        if (seatStatus[i - 1] == -1) {
+            i++;
+        } else {
+            cout << "|" << i << "|";
+            if (i % 10 == 0)
+                cout << endl;
+            i++;
+        }
+    }
+
+    cin >> newSeatNumber;
+    seatStatus[currentPassenger->seatNumber - 1] = 0;
+    currentPassenger->seatNumber = newSeatNumber;
+    seatStatus[newSeatNumber - 1] = -1;
 }
 
-// void Flight :: get_seat_map()
-// {
-// 	int i = 1;
-// 	while ( i < size+1 )
-// 	{
-// 		if ( seat[i-1] == -1)
-// 		cout <<"| " << i << "|";
-// 		else
-// 		cout <<"| " << "X" << "|";
-// 		if ( i%10 == 0 )
-// 		cout << endl;
-// 		i++;
-// 	}
-// }
-
-void Flight :: passenger_details()
-{
-	int reservation_number;
-	cout << "Enter your reservation number: ";
-	cin >> reservation_number;
-	temp_passenger = start;
-	while(temp_passenger != NULL)
-	{
-		if(temp_passenger->reservation_number == reservation_number)
-		{
-			cout << "Reservation Number\t\tFirst Name\t\tLast Name\t\tID\t\tPhone Number\t\tSeat Number\t\tFood Menu" << endl;
-			cout << temp_passenger->reservation_number << "\t\t\t\t" << temp_passenger->fname << "\t\t\t\t" << temp_passenger->lname << "\t\t\t\t" << temp_passenger->ID << "\t\t\t\t" << temp_passenger->phone_number << "\t\t\t\t" << temp_passenger->seat_number << "\t\t\t\t" << temp_passenger->food_menu << endl;
-			break;
-		}
-		else
-		{
-			temp_passenger = temp_passenger->next;
-		}
-	}
+void FlightReservation::showPassengerDetails() {
+    int reservationNumber;
+    cout << "Enter your reservation number: ";
+    cin >> reservationNumber;
+    tempPassenger = headPassenger;
+    while (tempPassenger != nullptr) {
+        if (tempPassenger->reservationNumber == reservationNumber) {
+            cout << "Reservation Number\tFirst Name\tLast Name\tID\tPhone Number\tSeat Number\tMeal Preference" << endl;
+            cout << tempPassenger->reservationNumber << "\t\t\t" << tempPassenger->firstName << "\t\t" << tempPassenger->lastName << "\t\t" << tempPassenger->id << "\t\t" << tempPassenger->phoneNumber << "\t\t" << tempPassenger->seatNumber << "\t\t" << tempPassenger->mealPreference << endl;
+            break;
+        }
+        tempPassenger = tempPassenger->next;
+    }
 }
 
-void Flight :: get_booking_details()
-{
-	temp_passenger = start;
-	cout << "Reservation Number\t\tFirst Name\t\tLast Name\t\tID\t\tPhone Number\t\tSeat Number\t\tFood Menu" << endl;
-	while(temp_passenger != NULL)
-	{
-		cout << temp_passenger->reservation_number << "\t\t\t\t" << temp_passenger->fname << "\t\t\t\t" << temp_passenger->lname << "\t\t\t\t" << temp_passenger->ID << "\t\t\t\t" << temp_passenger->phone_number << "\t\t\t\t" << temp_passenger->seat_number << "\t\t\t\t" << temp_passenger->food_menu << endl;
-		temp_passenger = temp_passenger->next;
-	}
+void FlightReservation::showAllBookings() {
+    tempPassenger = headPassenger;
+    cout << "Reservation Number\tFirst Name\tLast Name\tID\tPhone Number\tSeat Number\tMeal Preference" << endl;
+    while (tempPassenger != nullptr) {
+        cout << tempPassenger->reservationNumber << "\t\t\t" << tempPassenger->firstName << "\t\t" << tempPassenger->lastName << "\t\t" << tempPassenger->id << "\t\t" << tempPassenger->phoneNumber << "\t\t" << tempPassenger->seatNumber << "\t\t" << tempPassenger->mealPreference << endl;
+        tempPassenger = tempPassenger->next;
+    }
 }
 
-void welcome()
-{
-	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
-	std::cout << "\t\t|                                                                  |" << "\n"; 
-	std::cout << "\t\t|     WELCOME TO YRNCOLLO AIRLINE FLIGHT RESERVATION SYSTEM        |" << "\n";
-	std::cout << "\t\t|                                                                  |" << "\n"; 
-	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
-	std::cout << "\t\t|          Choose your option:                                     |" << "\n"; 
-	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
-	std::cout << "\t\t|                                                                  |" << "\n";  
-	std::cout << "\t\t|          1) BOOK TICKET                                          |" << "\n"; 
-	std::cout << "\t\t|                                                                  |" << "\n"; 
-	std::cout << "\t\t|          2) CANCEL TICKET                                        |" << "\n"; 
-	std::cout << "\t\t|                                                                  |" << "\n";
-	std::cout << "\t\t|          3) CHANGE RESERVATION                                   |" << "\n"; 
-	std::cout << "\t\t|                                                                  |" << "\n";
-	std::cout << "\t\t|          4) PASSENGER DETAILS                                    |" << "\n"; 
-	std::cout << "\t\t|                                                                  |" << "\n";
-	std::cout << "\t\t|          5) GET BOOKING DETAILS                                  |" << "\n"; 
-	std::cout << "\t\t|                                                                  |" << "\n";
-	std::cout << "\t\t|          6) EXIT                                                 |" << "\n"; 
-	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
+void displayMenu() {
+    cout << "\t\t|--------------------------------------------------------------|" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|  WELCOME TO YRNCOLLO AIRLINE FLIGHT RESERVATION SYSTEM     |" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|--------------------------------------------------------------|" << endl;
+    cout << "\t\t|  Choose your option:                                      |" << endl;
+    cout << "\t\t|--------------------------------------------------------------|" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|  1) BOOK TICKET                                           |" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|  2) CANCEL TICKET                                         |" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|  3) CHANGE RESERVATION                                    |" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|  4) VIEW PASSENGER DETAILS                                |" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|  5) VIEW ALL BOOKINGS                                     |" << endl;
+    cout << "\t\t|                                                            |" << endl;
+    cout << "\t\t|  6) EXIT                                                  |" << endl;
+    cout << "\t\t|--------------------------------------------------------------|" << endl;
 
-	int choice;
-	do
-	{
-		cout<<"Enter your choice: ";
-		cin>>choice;
-		switch(choice)
-		{
-			case 1:system("CLS");
-				flight.book_ticket();
-				break;
-			case 2:system("CLS");
-				flight.cancel_ticket();
-				break;
-			case 3:system("CLS");
-				flight.change_reservation();
-				break;
-			case 4:system("CLS");
-				flight.passenger_details();
-				break;
-			case 5:system("CLS");
-				flight.get_booking_details();
-				break;
-			case 6 : system("CLS");
-				exit(0);
-				break;
-			default:system("CLS");
-				cout<<"Invalid choice"<<endl;
-				break;
-		}
-	}while(choice!=7);
-}
-
-int main()
-{
-    welcome();
-    return 0;
-}
+    int choice;
+    do {
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                system("CLS");
+                flightReservation.bookTicket();
+                break;
+            case 2:
+                system("CLS");
+                flightReservation.cancelTicket();
+                break;
+            case 3
